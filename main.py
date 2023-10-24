d8e6d5094c99fe2ecfeb0788fcecee7c7b2d60c5
theKasten <t_kasten@gmx.de>
2023-10-24 13:03:41 +0200

    Added data generator

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import numpy as np
+import sklearn as sk
+from random import randint
+
+from sklearn.model_selection import train_test_split
+
 from perzeptron import Perzeptron
 
 #print("Hello World!")
@@ -16,36 +21,66 @@ w(z)? -> Gewicht??
 r -> lernrate
 """
 
-x_one = np.array([-1, 0])
-x_two = np.array([1, 0])
-x_three = np.array([1, 1])
-x = np.array([x_one, x_two])
-y_one = 0
-y_two = 1
-y_three = 1
-y = np.array([y_one, y_two])
-w = np.array([-1, 0])
-r = 1
+def aufgabe_01():
+    x_one = np.array([-1, 0])
+    x_two = np.array([1, 0])
+    x_three = np.array([1, 1])
+    x = np.array([x_one, x_two])
+    y_one = 0
+    y_two = 1
+    y_three = 1
+    y = np.array([y_one, y_two])
+    w = np.array([-1, 0])
+    r = 0.05
 
-p = Perzeptron(w, r)
+    p = Perzeptron(w, r)
 
-#Predict
-prediction = p.activating_function(x_three)
-print('Before Training Prediction for x three was class:' + str(prediction))
+    #Predict
+    prediction = p.activating_function(x_three)
+    print('Before Training Prediction for x three was class:' + str(prediction))
 
-#Train
-w = p.weight_new(x_one, y_one)
-print('New w is now:' + str(w))
-w = p.weight_new(x_two, y_two)
-print('New w is now:' + str(w))
+    #Train
+    w = p.weight_new(x_one, y_one)
+    print('New w is now:' + str(w))
+    w = p.weight_new(x_two, y_two)
+    print('New w is now:' + str(w))
 
 
-#Predict
-prediction = p.activating_function(x_three)
-print('Prediction for x three was class:' + str(prediction))
+    #Predict
+    prediction = p.activating_function(x_three)
+    print('Prediction for x three was class:' + str(prediction))
 
-print()
-print("----Now using Training Function----")
-w = np.array([-1, 0])
-p2 = Perzeptron(w, r)
-p2.train(x, y)
+    print()
+
+    x_test = np.array([[100, 5, -2],
+                       [100, -1, 30]])
+
+    print("----Now using Training Function----")
+    w = np.array([-1, 0])
+    p2 = Perzeptron(w, r)
+    p2.train(x, y)
+    print('Lables: ' + str(p2.infer(x_test)))
+
+    print()
+    print("---Now using Testing for training---")
+    p3 = Perzeptron(np.array([randint(-10, 10), randint(-10, 10)]), r)#Mit zufälligem w
+    p3.train(x_test, np.array([1, 1, 0]))
+    print('Lables: ' + str(p3.infer(x_test)))
+
+def aufgabe_02_03():
+    print('---Now Generating Data...---')
+    generated_data_matrix = Perzeptron.generate(10, 10)
+    print(generated_data_matrix)
+    generated_x = generated_data_matrix[:10, :];
+    generated_y = generated_data_matrix[10, :]
+    print('X: ' + str(generated_x))
+    print('Y: ' + str(generated_y))
+
+    print('---Starting Test Train Split---')
+    X_train, X_test, y_train, y_test = train_test_split(generated_x, generated_y, test_size=0.33, random_state=42)
+    print('X_train: ' + str(X_train))
+    print('X_test: ' + str(X_test))
+    print('y_train: ' + str(y_train))
+    print('y_test: ' + str(X_test))
+
+aufgabe_02_03() 
--- perzeptron.py
+++ perzeptron.py
@@ -1,13 +1,18 @@
+from random import randint
+
 import numpy as np
+from setuptools._vendor.more_itertools import seekable
 from setuptools._vendor.more_itertools.more import _sample_weighted
 
 
 class Perzeptron:
-    global w
-    global r
+    global w    #weight
+    global r    #lernrate
+    global b    #liasterm
     def __init__(self, w_init, learn_rate):
         self.w = w_init
         self.r = learn_rate
+        self.b = 0;
         print('Perzeptron has been initialised with initial weight of ' + str(self.w)
               + ' and initial learn rate ' + str(self.r) + '...')
 
@@ -16,20 +21,24 @@ class Perzeptron:
         INPUT
         x -> Eingabe Vektor
         RETURN
-        Soll Klasse ausgeben in das Perzeptron den Eingabevektor laut seinen internen Gewichten stecken würde
+        y -> Lables in das Perzeptron die Eingabevektoren laut seinen internen Gewichten stecken würde
         """
-        return self.activating_function(x)
+        y = np.zeros(x.shape[1])
+        for k in range(x.shape[1]):
+            y[k] = self.activating_function(x[:, k])
+
+        return y
 
     def activating_function(self, x):
         """
         INPUT:
         x -> Eingabe Vektor (einzelner Zeile mit Anzahl der Features(Spalten) vielen ausprägungen)
-        w -> Gewicht
+        w -> Gewich
         :return:
         Vorhersage der Binären klassifikation von x mit gewicht w
         (Also gehört x Klasse 0 oder 1 an)
         """
-        if np.dot(self.w, x) > 0:
+        if np.dot(self.w, x + self.b) > 0:
             #print("Element in klass 1")
             return 1
         else:
@@ -49,9 +58,28 @@ class Perzeptron:
         #w = w + np.dot(r * (y - self.activating_function(w, x)) , x)#TODO: So richtig??
 
     def train(self, x, y):
-        #x = np.array(x_in)
         #print("Shape x:" + str(x.shape))
-        for k in range(x.shape[1]):
-            self.weight_new(x[k, :], y[k])#TODO: So richtig? Müssen hier Reihen oder doch Spalten von X verwendet werden?
+        for k in range(x.shape[1]):#Schnappt sich Eizelne Zeilen aus der Matrix x. Die einzelnen Zeilen haben n Spalten/Features
+            self.weight_new(x[:, k], y[k])#TODO: So richtig? Müssen hier Zeilen oder doch Spalten von X verwendet werden?
             print('New w is now:' + str(self.w))
 
+    @staticmethod
+    def generate(number_of_features, dimension):
+        """Soll synthetische Daten generieren!"""
+        """Matrix mit n Feature vielen Zeilen + 1 Zeile für Lables, Dimension d/Anzahl der Spalten Heißt Anzahl der Eingabevektoren"""
+        #w_init = np.array([randint(-100, 100), randint(-100, 100)])
+        #Winit
+        w_init = np.zeros(number_of_features)
+        matrix = np.zeros((number_of_features + 1, dimension))# + 1 für lables
+        for n in range(number_of_features):
+            w_init[n] = np.array([randint(-100, 100)])
+
+        p = Perzeptron(w_init, 0.001)
+
+        for d in range(dimension):
+            for n in range(number_of_features + 1):
+                if n == number_of_features:
+                    matrix[n, d] = p.activating_function(matrix[:number_of_features, d])
+                else:
+                    matrix[n, d] = randint(-100, 100)
+        return matrix
