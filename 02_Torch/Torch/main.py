1de612f4a0a1d13ad55bf7baf0561d84f81a20f4
kasten <t_kasten@gmx.de>
2023-10-28 13:51:34 +0200

    Added Visualisierung für einfachen datensatz, functionen aus loesung eingebaut, Normierung

--- 02_Torch/Torch/main.py
+++ 02_Torch/Torch/main.py
@@ -1,1 +1,80 @@
-print('Hello!...')
+import numpy as np
+import matplotlib.pyplot as plt
+from perzeptron import Perzeptron
+from sklearn.datasets import load_iris
+from sklearn.decomposition import PCA
+
+def prep_perzeptron():
+    plt.rcParams['figure.figsize'] = (4, 4)
+
+    # Example data for binary classification:
+    xs = np.array([[1,0.3], [-1,1.1], [0,-1.1], [0.8, -0.9]], dtype='float64')
+    ys = np.array([0,       0,         0,        1], dtype='float64')
+
+    # Let's have a look at the data:
+    fig, ax = plt.subplots()
+    ax.grid()
+    ax.scatter(xs[:,0], xs[:,1], c=ys)
+    plt.show()
+
+    xs_aff = Perzeptron.to_affine(xs)
+    iterations = 0
+    loss = 1
+    #w = np.ones_like(xs_aff[0], dtype="float64")
+    my_perzeptron = Perzeptron()
+    my_perzeptron.train(xs_aff, ys)
+    w = my_perzeptron.weight
+
+    # Visualize Training
+    fig, ax = plt.subplots()
+    ax.grid()
+    ax.set(xlim = (-2, 2), ylim = (-2, 2))
+    ax.scatter(xs[:,0], xs[:,1], c=ys)
+
+    # compute decision boundary line:
+    bias, weight = -w[0], w[1:]
+    slope = - weight[0]/weight[1]
+    intercept = bias / weight[1]
+
+    ax.plot(boundary_x := np.linspace(-2, 2, 2), slope * boundary_x + intercept)
+    ax.arrow(bias/weight[0], 0, weight[0], weight[1],
+             width=.03, length_includes_head=True)
+
+    plt.show()
+
+def test_minibatch():
+    iterations = 0
+    loss = 1
+
+    my_perzeptron = Perzeptron()
+
+    # Example data for binary classification:
+    xs = np.array([[1,0.3], [-1,1.1], [0,-1.1], [0.8, -0.9]], dtype='float64')
+    ys = np.array([         0,      0,       0,          1])
+
+    while(loss > 0.1 and iterations < 500):
+        my_perzeptron.perceptron_fit_simd_minibatch(xs, ys,
+                                          learn_rate=0.01,
+                                          batchsize=1)
+        y_pred = my_perzeptron.activating_function(xs)
+        loss = np.sum(y_pred != ys) / len(ys)
+        if(iterations % 50 == 0): # only every 50th iteration:
+            print(iterations, "Gewichtsvektor: ", my_perzeptron.w)
+            print("Anteil falsch klassifiziert: ", loss)
+        iterations += 1
+
+        print("actual labels:    ", ys)
+        print("predicted labels: ", y_pred)
+
+def evaluation_auf_Iris:
+    xs, ys = load_iris(return_X_y=True)
+    xs, ys = xs[ys != 2], ys[ys != 2]
+    xs = PCA(2).fit_transform(xs)
+
+    plot_prediction_with_boundary(xs, ys, epochs=4)
+
+
+
+#prep_perzeptron()
+#test_minibatch()
+evaluation_auf_Iris()
--- 02_Torch/Torch/perzeptron.py
+++ 02_Torch/Torch/perzeptron.py
@@ -3,27 +3,27 @@ from random import randint
 import numpy as np
 
 class Perzeptron:
-    global w    #weight
-    global r    #lernrate
-    global b    #liasterm
+    global weight    #weight
+    global mute
+    #global r    #lernrate
 
     @staticmethod
     def to_affine(x):
-        return np.append(np.ones_like(x[:, 0]).reshape(-1, 1), x, axis=1)
-
-    def __init__(self, w_init, learn_rate):
-        self.w = w_init
-        self.r = learn_rate
-        self.b = 0;
+        return np.append(np.ones_like(x[:,0]).reshape(-1,1), x, axis=1)
+    def __init__(self):
+        self.mute = False
+        self.weight = None
+        #self.r = None
+        #self.w = np.ones(1, dtype="float64")
         #print('Perzeptron has been initialised with initial weight of ' + str(self.w)
         #      + ' and initial learn rate ' + str(self.r) + '...')
 
     def infer(self, x):
         """
         INPUT
-        x -> Eingabe Vektor
+        x → Eingabe Vektor
         RETURN
-        y -> Lables in das Perzeptron die Eingabevektoren laut seinen internen Gewichten stecken würde
+        y → Lables in das Perzeptron die Eingabevektoren laut seinen internen Gewichten stecken würde
         """
         y = np.zeros(x.shape[1], dtype=int)
         for k in range(x.shape[1]):
@@ -34,20 +34,22 @@ class Perzeptron:
     def activating_function(self, x):
         """
         INPUT:
-        x -> Eingabe Vektor (einzelner Zeile mit Anzahl der Features(Spalten) vielen ausprägungen)
-        w -> Gewich
+        x → Eingabe Vektor (einzelner Zeile mit Anzahl der Features(Spalten) vielen ausprägungen)
+        w → Gewich
         :return:
         Vorhersage der Binären klassifikation von x mit gewicht w
         (Also gehört x Klasse 0 oder 1 an)
         """
-        if np.dot(self.w, x + self.b) > 0:
+        #print("w, x: " + str(self.weight) + str(x))
+        if np.dot(self.weight, x) > 0:
             #print("Element in klass 1")
             return 1
         else:
             #print("Element in klass 0")
             return 0
+        # Besser: #return np.where(np.tensordot(x, self.w, axes=1) > 0, 1, 0)
 
-    def weight_new(self, x, y):
+    def weight_new(self, x, y, learn_rate):
         """
         INPUT:
         x -> Eingabe Vektor (einzelner Zeile mit Anzahl der Features(Spalten) vielen ausprägungen)
@@ -56,14 +58,22 @@ class Perzeptron:
         r -> lernrate
         :return:
         """
-        self.w = (self.w + self.r * (y - self.activating_function(x)) * x)#TODO: So richtig??
-        #w = w + np.dot(r * (y - self.activating_function(w, x)) , x)#TODO: So richtig??
+        self.show_information("Calc new weight based on: ")
+        self.weight = self.weight/np.linalg.norm(self.weight)
+        self.show_information("Weight now normed: ")
+        self.weight += learn_rate * (y - self.activating_function(x)) * x#Hier += vergessen!!
+        self.show_information("weight update done: ")
 
-    def train(self, x, y):
+    def train(self, xs, ys, learn_rate = 0.01):
         #print("Shape x:" + str(x.shape))
-        for k in range(x.shape[0]):#Schnappt sich Eizelne Zeilen aus der Matrix x. Die einzelnen Zeilen haben n Spalten/Features
-            self.weight_new(x[k, :], y[k])#TODO: So richtig? Müssen hier Zeilen oder doch Spalten von X verwendet werden?
-            #print('New w is now:' + str(self.w))
+        self.weight = np.ones_like(xs[0], dtype="float64")
+        for x, y in zip(xs, ys):
+            #self.show_information()
+            #print("Now Calc new weight")
+            self.weight_new(x, y, learn_rate)
+            #print("Done with new weight")
+            #self.show_information()
+            #self.w = self.w/np.linalg.norm(self.w)#Normieren damit gewicht nicht zu klein wird
 
     @staticmethod
     def generate(number_of_features, dimension):
@@ -76,7 +86,7 @@ class Perzeptron:
         for n in range(number_of_features):
             w_init[n] = np.array([randint(-100, 100)])
 
-        p = Perzeptron(w_init, 0.001)
+        p = Perzeptron()
 
         for d in range(dimension):
             for n in range(number_of_features + 1):
@@ -86,6 +96,33 @@ class Perzeptron:
                     matrix[n, d] = randint(-100, 100)
         return matrix
 
-    def show_information(self):
-        print('Perzeptron has weight of ' + str(self.w)
-              + ' and learn rate ' + str(self.r) + '...')
+    def show_information(self, information):
+        if not self.mute:
+            print(information)
+            print('Perzeptron has weight of ' + str(self.weight)
+                  + ' and learn rate ' + str(None) + '...')
+        #do nothing
+
+    """def perceptron_fit_simd_minibatch(self, xs, ys, learn_rate=0.01, batchsize=1):
+        weight_vector=np.ones_like(xs[0], dtype='float64')
+        for batch in range(len(xs)//batchsize):
+            xs_batch = xs[batch*batchsize:(batch+1)*batchsize]
+            ys_batch = ys[batch*batchsize:(batch+1)*batchsize]
+            self.weight += learn_rate * np.sum((ys_batch \
+                                                - self.perceptron_predict_simd2(xs_batch))
+                                               * xs_batch,
+                                               axis=0)"""
+
+    def perceptron_fit_simd_minibatch(self, xs, ys, learn_rate=0.01, batchsize=1):
+        weight_vector = np.ones_like(xs[0], dtype="float64")
+        for batch in range(len(xs)//batchsize):
+            xs_batch = xs[batch*batchsize:(batch+1)*batchsize]
+            ys_batch = ys[batch*batchsize:(batch+1)*batchsize]
+            weight_vector += learn_rate * np.sum( (ys_batch \
+                                                   - self.perceptron_predict_simd2(weight_vector, xs_batch))
+                                                  * xs_batch,
+                                                  axis=0)
+        self.weight = weight_vector
+
+    def perceptron_predict_simd2(self, weight_vector, xs):
+        return np.where(np.tensordot(xs, weight_vector, axes=1) > 0, 1, 0)
